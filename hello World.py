00d6d9488c15b0f5e958db91a9d9889a22e3a0f9
Naveenkumar Gumaste <64587607+BrownUchiha@users.noreply.github.com>
2023-08-18 19:44:21 +0530

    testing Changes

--- hello World.py
+++ hello World.py
@@ -1,1 +1,13 @@
-print("Hello, world!")
+A = 5
+B = 6
+C = 7
+
+diff = A-B
+diff2 = A-B-C
+Product = A*B
+Product2 = B*C
+
+print("Sub of A & B", diff)
+print("Sub of A & B & C", diff2)
+print("Mul of A & B", Product)
+print("Mul of B & C", Product2)
